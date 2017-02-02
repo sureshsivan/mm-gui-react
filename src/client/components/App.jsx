73aae2517c0bde3bae62151a6b52507a879d08db
Suresh Sivanantham <sivanantham.suresh@nielsen.com>
2017-02-02 17:00:19 -0500

    React semantic library Added

--- src/client/components/App.jsx
+++ src/client/components/App.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Header } from 'semantic-ui-react';
 
 export default class App extends React.Component {
     render() {
         return (
-            <div style={{textAlign: 'center'}}>
-                <h1>Hello World from React</h1>
-            </div>);
+            <div>
+                <Header as='h2'>Header Through React Component</Header>
+            </div>
+            // <div style={{textAlign: 'center'}}>
+            //     <h1>Hello World from React</h1>
+            // </div>
+        );
     }
 }
