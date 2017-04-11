81056d9a876a6c400c6aa58f0f591bdee4613195
Suresh Sivanantham <cliksuresh18@gmail.com>
2017-04-11 14:46:24 +0000

    Adding redux debugger

--- src/app/components/App.component.jsx
+++ src/app/components/App.component.jsx
@@ -9,7 +9,7 @@ import { Provider } from 'react-redux';
 export default class App extends React.Component {
     render() {
         return (
-            <Provider store={createStore(reducers)}>
+            <Provider store={createStore(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())}>
                 <TopBar/>
             </Provider>
         );
